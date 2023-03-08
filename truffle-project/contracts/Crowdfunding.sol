f9ad17706df23650ccc1782ddc4cbb3e30ab525a
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-07 21:06:36 -0600

    Define contract's public properties

--- truffle-project/contracts/Crowdfunding.sol
+++ truffle-project/contracts/Crowdfunding.sol
@@ -2,6 +2,20 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Crowdfunding {
-  constructor() public {
-  }
+  enum State { Ongoing, Failed, Succeded, PaidOut }
+  
+  // declare the deployed contract name
+  string public name;
+
+  // the funding campaign amount goal
+  uint public targetAmount;
+
+  // keep the date till which the campaign is valid
+  uint public fundingDeadline;
+
+  // where to send the funds if campaign is succeded
+  address payable public beneficiary;
+
+  // current campaign state
+  State public state;
 }

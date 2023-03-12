39cd17fa9f9a5df951359a397b508b19416270d2
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-11 20:25:08 -0600

    Make the Casmpaign page fully working

--- web-app/src/components/Campaign.js
+++ web-app/src/components/Campaign.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo } from 'react'
 import { Button, Table, Message } from 'semantic-ui-react'
 import {
-  useParams,
+    useParams,
 } from 'react-router-dom'
 
 import { getWeb3 } from '../ethereum/utils'
@@ -16,7 +16,9 @@ const FAILED_STATE = '1'
 const SUCCEEDED_STATE = '2'
 
 export default function Campaign() {
-
+    const web3 = useMemo(() => getWeb3(), [])
+    const [currentAccount, setCurrentAccount] = useState(null)
+    const [networkId, setNetworkId] = useState(null)
     const [contractInfo, setContractInfo] = useState({
         name: 'N/A',
         targetAmount: 0,
@@ -27,6 +29,91 @@ export default function Campaign() {
         contributedAmount: 10,
         state: ONGOING_STATE
     })
+
+    async function connectWallet() {
+        const accounts = await web3.eth.requestAccounts()
+        setCurrentAccount(accounts[0])
+    }
+
+    async function getCurrentConnectedAccount() {
+        const accounts = await web3.eth.getAccounts()
+        setCurrentAccount(accounts[0])
+        setNetworkId(await web3.eth.net.getId())
+    }
+
+    window.ethereum.on('accountsChanged', function (accounts) {
+        if (accounts) {
+            setCurrentAccount(accounts[0])
+        }
+    })
+
+    window.ethereum.on('networkChanged', function (networkId) {
+        console.log("New network ID: ", networkId)
+        setNetworkId(networkId)
+    })
+
+    const { address } = useParams();
+
+    useEffect(() => {
+        getCurrentConnectedAccount()
+    })
+
+    useEffect(() => {
+        async function getCampaign(address) {
+            if (!currentAccount) return;
+
+            const contract = getContract(web3, address)
+
+            try {
+                const name = await contract.methods.name().call()
+                const targetAmount = await contract.methods.targetAmount().call()
+                const totalCollected = await contract.methods.totalCollected().call()
+                const beforeDeadline = await contract.methods.beforeDeadline().call()
+                const beneficiary = await contract.methods.beneficiary().call()
+                const deadlineSeconds = await contract.methods.fundingDeadline().call()
+                const contributedAmount = await contract.methods.amounts(currentAccount).call()
+                const state = await contract.methods.state().call()
+
+                var deadlineDate = new Date(0)
+                deadlineDate.setUTCSeconds(deadlineSeconds)
+
+                setContractInfo({
+                    name: name,
+                    targetAmount: targetAmount,
+                    totalCollected: totalCollected,
+                    campaignFinished: !beforeDeadline,
+                    deadline: deadlineDate,
+                    isBeneficiary: beneficiary.toLowerCase() === currentAccount.toLowerCase(),
+                    contributedAmount: contributedAmount,
+                    state: state
+                })
+            } catch (e) {
+                setContractInfo(null)
+            }
+        }
+        getCampaign(address)
+    }, [web3, address, currentAccount, networkId])
+
+    if (!currentAccount) {
+        return (
+            <>
+                <Message info>
+                    <Message.Header>Website is not connected to Ethereum</Message.Header>
+                    <p>You need to connect your wallet first</p>
+                </Message>
+                <Button primary onClick={() => connectWallet()}>Connect Wallet</Button>
+            </>
+        )
+    }
+
+    if (!contractInfo) {
+        return (
+            <Message negative>
+                <Message.Header>Failed to load contract data</Message.Header>
+                <p>Check if the contract is deployed and you are using the right network</p>
+            </Message>
+        )
+    }
 
     return <Table celled>
         <Table.Header>
@@ -78,8 +165,28 @@ export default function Campaign() {
         <Table.Footer fullWidth>
             <Table.Row>
                 <Table.HeaderCell colSpan="2">
+                    {campaignInteractionSection(contractInfo, address, currentAccount)}
                 </Table.HeaderCell>
             </Table.Row>
         </Table.Footer>
     </Table>
+}
+
+
+function campaignInteractionSection(contractInfo, address, currentAccount) {
+    if (contractInfo.state === ONGOING_STATE) {
+        return <ContributeInput
+            campaignFinished={contractInfo.campaignFinished}
+            contractAddress={address}
+            currentAccount={currentAccount}
+        />
+    } else if (contractInfo.state === SUCCEEDED_STATE) {
+        return <SuccededStateInput
+            isBeneficiary={contractInfo.isBeneficiary}
+        />
+    } else if (contractInfo.state === FAILED_STATE) {
+        return <FailedStateInput
+            contributedByCurrentAccount={contractInfo.contributedByCurrentAccount}
+        />
+    }
 }
